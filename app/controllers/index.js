8252499c51aec68fda9d285fa13b46a9b645f365
Devon Anderson <dvonanderson@gmail.com>
2019-03-25 13:42:06 -0400

    building out app

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,17 @@
 import Controller from '@ember/controller';
-
+import { match, not } from '@ember/object/computed';
 export default Controller.extend({
+
+  emailAddress: '',
+  responseMessage: '',
+  isValid: match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: not('isValid'),
+
+  actions: {
+    saveInvitation(){
+      alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
+      this.set('responseMessage', `Thank You! We've just saved your email address: ${this.get('emailAddress')}`);
+      this.set('emailAddress', '');
+    }
+  }
 });
